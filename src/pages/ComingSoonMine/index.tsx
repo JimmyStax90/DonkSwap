e83120aedc63d3faba33b263685283bc0f59257b
pmar0 <pmarino1994@gmail.com>
2022-06-20 16:36:42 -0400

    [fix]: fixed withdraw to display token amt + misc

--- src/pages/ComingSoonMine/index.tsx
+++ src/pages/ComingSoonMine/index.tsx
@@ -1,7 +1,8 @@
 import "./stake.css";
 import React, { useState, useRef } from 'react'
 import useStaking from "../../hooks/useStaking";
-import { ethers, BigNumber } from "ethers"
+import { ethers } from 'ethers';
+import { BigNumber } from 'bignumber.js';
 import useERC20 from "hooks/useERC20";
 import { useActiveWeb3React } from '../../hooks/index';
 import ConnectWalletButton from 'components/ConnectWalletButton';
@@ -11,10 +12,12 @@ import useI18n from 'hooks/useI18n';
 
 export default function Mine() {
   const { account } = useActiveWeb3React();
-  const [depositinputamount, setDepositInputAmount] = useState(0.00);
-  const [withdrawalinputamount, setWithdrawalInputAmount] = useState(0);
-  const [claiminputamount, setClaimInputAmount] = useState(0);
-  const [errormessage, setErrorMessage] = useState('')
+  const [depositinputamount, setDepositInputAmount] = useState<string>('');
+  const [withdrawalinputamount, setWithdrawalInputAmount] = useState<string>('');
+  const [claiminputamount, setClaimInputAmount] = useState<string>('');
+  const [errormessagewithdraw, setErrorMessagewithdraw] = useState('')
+  const [errormessagedeposit, setErrorMessagedeposit] = useState('')
+  const [errormessageclaim, setErrorMessageclaim] = useState('')
   const { stakedbalance, earnedBalance, totalStakedBalance, totalRewards, rewardRate, stake,restake, withdraw, claimReward } = useStaking();
   const { allowance, approve, balance } = useERC20();
   const total = Number(ethers.utils.formatEther(totalStakedBalance?totalStakedBalance:0));
@@ -41,31 +44,42 @@ export default function Mine() {
   /*======================= ClaimReward ===================================*/
 
   const ClaimInput = (e) => {
-    if (e.target.value === '' || e.target.value < 0)
+    setErrorMessageclaim('');
+    const amt = new BigNumber(e.target.value);
+    if (amt.lt(0))
     {
-      setClaimInputAmount(0);
+      setClaimInputAmount('0');
     } else {
-      setClaimInputAmount(parseInt(e.target.value));
+      setClaimInputAmount(amt.toFixed(0));
     }
   }
   const incrementClaimUp = () => {
-    setClaimInputAmount(parseInt((claiminputamount + 1).toFixed(0)));
+    setErrorMessageclaim('');
+    const amt = new BigNumber(claiminputamount).plus(1);
+    setClaimInputAmount(amt.toFixed(0));
     if(claimInputRef.current) {
       claimInputRef.current.focus();
     }
   }
   const decrementClaimDown = () => {
-    if (claiminputamount - 1 < 0) {
-      setClaimInputAmount(0);
+    setErrorMessageclaim('');
+    const amt = new BigNumber(claiminputamount).minus(1);
+    if (amt.lt(0) || amt.isNaN()) {
+      setClaimInputAmount('0');
     } else {
-      setClaimInputAmount(parseInt((claiminputamount - 1).toFixed(0)));
+      setClaimInputAmount(amt.toFixed(0));
     }
     if(claimInputRef.current) {
       claimInputRef.current.focus();
     }
   }
   const handleClickClaim = () => {
-    claimReward(ethers.utils.parseEther(claiminputamount.toString()))
+    const userRewardBal = new BigNumber(ethers.utils.formatUnits(earnedBalance,9).slice(0, ethers.utils.formatUnits(earnedBalance,9).indexOf(".")+3))
+    if(new BigNumber(claiminputamount).gt(userRewardBal)) {
+      setErrorMessageclaim('Amount cannot exceed your reward balance.')
+    } else {
+      claimReward(ethers.utils.parseEther(claiminputamount.toString()))
+    }
   }
 
   const handleClickMaxClaimDropdown = () => {
@@ -82,8 +96,8 @@ export default function Mine() {
     if (!hasUserApproved && approveBtnRef.current) {
       approveBtnRef.current.classList.toggle('shake');
     } else {
-      const userBal = Number(ethers.utils.formatEther(balance).slice(0, ethers.utils.formatEther(balance).indexOf(".")+3));
-      setClaimInputAmount(Number((userBal*(percent/100)).toFixed(2)));
+      const userRewardBal = new BigNumber(ethers.utils.formatUnits(earnedBalance,9).slice(0, ethers.utils.formatUnits(earnedBalance,9).indexOf(".")+3))
+      setClaimInputAmount((userRewardBal.times(percent/100)).toFixed(0));
     }
 
     if(maxDropdownContentClaimRef.current && overlayRef.current) {
@@ -99,24 +113,39 @@ export default function Mine() {
   /*======================= DEPOSIT ===================================*/
   /*======================= DEPOSIT ===================================*/
 
-  const depositInput = (e) => {
-    if (e.target.value === '' || e.target.value < 0) {
-      setDepositInputAmount(0.00);
+  const handleClickStake = () => {
+    const userBal = new BigNumber(ethers.utils.formatEther(balance?balance:0).slice(0, ethers.utils.formatEther(balance?balance:0).indexOf(".")+3));
+    if (new BigNumber(depositinputamount).gt(userBal)){
+      setErrorMessagedeposit('Amount cannot exceed your balance.')
     } else {
-      setDepositInputAmount(parseFloat(e.target.value));
+      stake(ethers.utils.parseEther(depositinputamount))
+    }
+  }
+
+  const depositInput = (e) => {
+    setErrorMessagedeposit('');
+    const amt = new BigNumber(e.target.value);
+    if (amt.lt(0)) {
+      setDepositInputAmount('0.000');
+    } else {
+      setDepositInputAmount(amt.toString());
     }
   }
   const incrementDepositUp = () => {
-    setDepositInputAmount(parseInt((depositinputamount + 1).toFixed(2)));
+    setErrorMessagedeposit('');
+    const amt = new BigNumber(depositinputamount).plus(1)
+    setDepositInputAmount(amt.toString());
     if(depositInputRef.current) {
       depositInputRef.current.focus();
     }
   }
   const decrementDepositDown = () => {
-    if (depositinputamount - 1 < 0) {
-      setDepositInputAmount(0.00);
+    setErrorMessagedeposit('');
+    const amt = new BigNumber(depositinputamount).minus(1)
+    if (amt.lt(0) || amt.isNaN()) {
+      setDepositInputAmount('0.000');
     } else {
-      setDepositInputAmount(parseInt((depositinputamount - 1).toFixed(2)));
+      setDepositInputAmount(amt.toString());
     }
     if(depositInputRef.current) {
       depositInputRef.current.focus();
@@ -137,8 +166,8 @@ export default function Mine() {
     if (!hasUserApproved && approveBtnRef.current) {
       approveBtnRef.current.classList.toggle('shake');
     } else {
-      const userBal = Number(ethers.utils.formatEther(balance).slice(0, ethers.utils.formatEther(balance).indexOf(".")+3));
-      setDepositInputAmount(Number((userBal*(percent/100)).toFixed(2)));
+      const userBal = new BigNumber(ethers.utils.formatEther(balance).slice(0, ethers.utils.formatEther(balance).indexOf(".")+3));
+      setDepositInputAmount((userBal.times(percent/100)).toFixed(3));
     }
 
     if(maxDropdownContentDepositRef.current && overlayRef.current) {
@@ -155,32 +184,43 @@ export default function Mine() {
   /*======================= WITHDRAWAL===================================*/
 
   const withdrawalInput = (e) => {
-    if (e.target.value === '' || e.target.value < 0) {
-      setWithdrawalInputAmount(0.000);
+    setErrorMessagewithdraw('');
+    const amt = new BigNumber(e.target.value);
+    if (amt.lt(0)) {
+      setWithdrawalInputAmount('0');
     } else {
-      setWithdrawalInputAmount(parseFloat(e.target.value));
+      setWithdrawalInputAmount(amt.toString());
     }
   }
   const incrementWithdrwalUp = () => {
-    setWithdrawalInputAmount(parseFloat((withdrawalinputamount + 1).toFixed(3)));
+    setErrorMessagewithdraw('');
+    const amt = new BigNumber(withdrawalinputamount).plus(1);
+    setWithdrawalInputAmount(amt.toString());
     if(withdrawInputRef.current) {
       withdrawInputRef.current.focus();
     }
   }
   const decrementWithdrawalDown = () => {
-    if (withdrawalinputamount - 1 < 1) {
-      setWithdrawalInputAmount(1);
+    setErrorMessagewithdraw('');
+    const amt = new BigNumber(withdrawalinputamount).minus(1)
+    if (amt.lt(0) || amt.isNaN()) {
+      setWithdrawalInputAmount('0');
     } else {
-      setWithdrawalInputAmount(parseFloat((withdrawalinputamount - 1).toFixed(3)));
+      const amt = new BigNumber(withdrawalinputamount).minus(1)
+      setWithdrawalInputAmount(amt.toString());
     }
     if(withdrawInputRef.current) {
       withdrawInputRef.current.focus();
     }
   }
   const handleClickWithdraw = () => {
-    const userStakedBal = Number(ethers.utils.formatEther(stakedbalance).slice(0, ethers.utils.formatEther(stakedbalance).indexOf(".")+3));
-    const withdrawPercent = (withdrawalinputamount/userStakedBal)*100;
-    withdraw(withdrawPercent.toString())
+    const userStakedBal = new BigNumber(ethers.utils.formatEther(stakedbalance).slice(0, ethers.utils.formatEther(stakedbalance).indexOf(".")+3));
+    if (new BigNumber(withdrawalinputamount).gt(userStakedBal)) {
+      setErrorMessagewithdraw('Amount cannot exceed your staked balance.')
+    } else {
+      const withdrawPercent = (new BigNumber(withdrawalinputamount).div(userStakedBal)).times(100);
+      withdraw(withdrawPercent.toString())
+    }
   }
 
   const handleClickMaxWithdrawDropdown = () => {
@@ -198,7 +238,8 @@ export default function Mine() {
       approveBtnRef.current.classList.toggle('shake');
     } else {
       const userStakedBal = Number(ethers.utils.formatEther(stakedbalance).slice(0, ethers.utils.formatEther(stakedbalance).indexOf(".")+3));
-      setWithdrawalInputAmount(Number((userStakedBal*(percent/100)).toFixed(3)));
+      const amt = new BigNumber(userStakedBal*(percent/100))
+      setWithdrawalInputAmount(amt.toFixed(3));
     }
 
     if(maxDropdownContentWithdrawRef.current && overlayRef.current) {
@@ -217,12 +258,12 @@ export default function Mine() {
     approve()
   }
 
-  const handleClickStake = () => {
-    stake(ethers.utils.parseEther(depositinputamount.toString()))
-  }
-
-  const handleClickReStake = () => {
-    restake()
+  const handleClickReStake = async () => {
+    try {
+      await restake()
+    } catch (e) {
+      console.log(e)
+    }
   }
 
   const handleClickOverlay = () => {
@@ -318,13 +359,18 @@ export default function Mine() {
                   <span className="max-btn" onClick={() => account ? handleClickMaxDeposit(25):null}>25%</span>
                 </div>
               </div>
-              <input ref={depositInputRef} disabled={!account} onChange={depositInput} value={depositinputamount.toString()} type="number" placeholder='0.0' className="deposit-input" />
+              <input ref={depositInputRef} disabled={!account} onChange={depositInput} value={depositinputamount} type="number" placeholder='0.000' className="deposit-input" />
             </div>
             <div className="increment-div">
               <button disabled={!account} onClick={incrementDepositUp} className="increment-plus">+</button>
               <button disabled={!account} onClick={decrementDepositDown} className="increment-minus">-</button>
             </div>
           </div>
+          {
+            errormessagedeposit?
+            <p className='errorMsg'>{errormessagedeposit}</p>
+            : null
+          }
           {
             account?
               hasUserApproved?
@@ -357,7 +403,7 @@ export default function Mine() {
                   <span className="max-btn" onClick={() => account ? handleClickMaxWithdraw(25):null}>25%</span>
                 </div>
               </div>
-              <input ref={withdrawInputRef} disabled={!account} onChange={withdrawalInput} value={withdrawalinputamount.toString()} type="number" placeholder="0.00" className="withdrawal-input" />
+              <input ref={withdrawInputRef} disabled={!account} onChange={withdrawalInput} value={withdrawalinputamount} type="number" placeholder="0.000" className="withdrawal-input" />
             </div>
               {/* <span className="percent-symbol">%</span> */}
             <div className="increment-div">
@@ -366,8 +412,13 @@ export default function Mine() {
             </div>
           </div>
           {
+            errormessagewithdraw?
+            <p className='errorMsg'>{errormessagewithdraw}</p>
+            : null
+          }
+          {
             account?
-              <button className={`stake-btn ${!hasStakedBal || withdrawalinputamount === 0 ? 'btn-disabled':''}`} disabled={!account || !hasStakedBal || withdrawalinputamount === 0} onClick={handleClickWithdraw}>Withdraw</button>
+              <button className={`stake-btn ${hasStakedBal || parseFloat(withdrawalinputamount) === 0 || withdrawalinputamount === '' ? 'btn-disabled':''}`} disabled={!account || !hasStakedBal || parseFloat(withdrawalinputamount) === 0 ||  withdrawalinputamount === ''} onClick={handleClickWithdraw}>Withdraw</button>
               : <ConnectWalletButton style={{
                   width: '170px',
                   height: '35px',
@@ -392,7 +443,7 @@ export default function Mine() {
                   <span className="max-btn" onClick={() => account ? handleClickMaxClaim(25):null}>25%</span>
                 </div>
               </div>
-              <input ref={claimInputRef} disabled={!account} onChange={ClaimInput} value={claiminputamount.toString()} type="number" placeholder="0" className="withdrawal-input" />
+              <input ref={claimInputRef} disabled={!account} onChange={ClaimInput} value={claiminputamount} type="number" placeholder="0" className="withdrawal-input" />
             </div>
             <div className="increment-div">
               <button disabled={!account} onClick={incrementClaimUp} className="increment-plus">+</button>
@@ -400,8 +451,13 @@ export default function Mine() {
             </div>
           </div>
           {
+            errormessageclaim?
+            <p className='errorMsg'>{errormessageclaim}</p>
+            : null
+          }
+          {
             account?
-              <button className={`stake-btn ${!hasEarnedBal ? 'btn-disabled':''}`} disabled={!account || !hasEarnedBal} onClick={handleClickClaim}>Claim</button>
+              <button className={`stake-btn ${!hasEarnedBal ? 'btn-disabled':''}`} disabled={!account || !hasEarnedBal || parseInt(claiminputamount) === 0 || claiminputamount === '' } onClick={handleClickClaim}>Claim</button>
               : <ConnectWalletButton style={{
                 width: '170px',
                 height: '35px',
