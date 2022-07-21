6618fbef199ea8302948b7bf543c2befff95ec77
FanbaseEU <Ronald.mears@mearsmediagroup.com>
2022-07-21 16:40:36 +0300

    update mobile

--- src/pages/DonkNFT/index.tsx
+++ src/pages/DonkNFT/index.tsx
@@ -11,12 +11,13 @@ export default function DonkNFT() {
         }
       `}</style>
             <div className="container_100">
-            <div  style={{width: '100%', height: '100%'}}>
-                {/*<img src={(aceSVG)} alt="ace" className="ace-logo" />*/}
-                <img src='/images/logo.png' alt="logo" style={{position: 'relative'}} className="animated-logo" />
-            </div>
+                <div style={{width: '100%', height: '100%'}}>
+                    {/*<img src={(aceSVG)} alt="ace" className="ace-logo" />*/}
+                    <img src='/images/logo.png' alt="logo" style={{position: 'relative'}} className="animated-logo"/>
+                </div>
                 <h2 style={{textAlign: 'center'}}>DonkSwap NFT Launchpad</h2>
                 <h3 style={{textAlign: 'center'}}>Coming Soon...</h3>
+            </div>
         </>
     )
 }
