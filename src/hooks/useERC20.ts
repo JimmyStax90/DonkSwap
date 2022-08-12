5ddf226842a89be55569c93e45fee5cb32becdfd
Plastic Digits <83603340+PlasticDigits@users.noreply.github.com>
2022-08-11 19:31:27 -0700

    Update useErc20 staking address

--- src/hooks/useERC20.ts
+++ src/hooks/useERC20.ts
@@ -7,7 +7,7 @@ import { BigNumber, ethers } from 'ethers';
 const contractAddress = "0x7123431162c1efF257578D1574014e5305Eb7bd4";
 
 // const stakingAddress = "0x8cfE605A4Cebef5c9C69B167d9A99B21BBD53C72";
-const stakingAddress = "0x705dA527fA74270971F3f2b432825217f62b7E5E";
+const stakingAddress = "0x3f51810e9A449A95Ef4fC52C956BcB62a291D3AD";
 
 
 const useERC20 = () => {
