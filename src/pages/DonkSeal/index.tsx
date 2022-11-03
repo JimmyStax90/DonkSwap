82e14179dce1fede30c5403694947786345b9a40
Jimmy Stax <87676344+JimmyStax90@users.noreply.github.com>
2022-11-03 12:43:12 -0700

    Update index.tsx

--- src/pages/DonkSeal/index.tsx
+++ src/pages/DonkSeal/index.tsx
@@ -55,7 +55,7 @@ export default function DonkSeal() {
             coinName: 'DonkSwap Token (DST)',
             coinAddress: '0x3969Fe107bAe2537cb58047159a83C33dfbD73f9',
             coinLogo:
-              'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/smartchain/assets/0x1F82a9b541FE4C322B5cd81DCB066620D57de35C/logo.png',
+              'https://i.ibb.co/1XPhwnW/DK-Chip-No-BG.png',
             coinWebsite: 'https://donk.finance',
             auditFile: 'https://github.com/Watchtower-WTW/Public_Audits/blob/main/DONKSWAP%20Smart%20Contract%20Security%20Audit%20(2).pdf',
             auditLogo: 'https://i.ibb.co/jHTbY4v/Watchtower.jpg',
